a28fa6e307ea9573a13aeb3aa99e90724641bf5b
DerekHayden <dmeadows@kerio.com>
2016-05-13 16:14:54 +1000

    Update hello.c
    Program can now do a custom greet from the command line. Also un-fixed logic bug that was not actually an error.

--- hello.c
+++ hello.c
@@ -4,10 +4,10 @@
 int
 main(int argc, char **argv) {
     
-    if (argc > 0) {
-        printf("%s\n", argv[1])   
+    if (argc > 1) {
+        printf("Hello %s!\n", argv[1]);   
+    } else {
+        printf("Hello World!\n");
     }
-    
-    printf("Hello World!\n");
     return 0;
 }
